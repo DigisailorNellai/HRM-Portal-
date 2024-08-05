e266142e27d04790aad01610b0588b2202e1df01
Ramanan1711 <ajayramanan17@gmail.com>
2024-08-05 15:32:13 +0530

    Your commit message

---
+++ HRM-Portal/src/test/java/com/example/HRM_Portal/HrmPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HRM_Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
